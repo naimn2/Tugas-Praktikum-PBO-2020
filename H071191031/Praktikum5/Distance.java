772892b39e64f7dcefe76988b12ece6570360e36
andiarioo <arioandiid2812@gmail.com>
2020-05-29 20:52:43 +0800

    Praktikum 5

---
+++ H071191031/Praktikum5/Distance.java
@@ -0,0 +1,7 @@
+//Distance
+public interface Distance {
+
+    public double metreToKilo(double value);
+    public double metreToMilli(double value);
+    public double inchToMetre(double value);
+}
---
+++ H071191031/Praktikum5/Temperature.java
@@ -0,0 +1,6 @@
+//Temperature
+public interface Temperature {
+    public double celciusToFahrenheit(double value);
+    public double celciusToKelvin(double value);
+
+}
