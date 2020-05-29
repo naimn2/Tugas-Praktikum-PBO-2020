f4c56dee013da84db9c8220a20de5c8e3c883201
andiarioo <arioandiid2812@gmail.com>
2020-05-29 13:29:42 +0800

    praktikum 1

---
+++ H071191031/Praktikum1/MainRectangle.java
@@ -0,0 +1,8 @@
+class MainRectangle{
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle();
+        rectangle1.height = 13.4;
+        rectangle1.width = 12.23;
+        System.out.println(rectangle1.getArea());
+    }
+}
---
+++ H071191031/Praktikum1/Person.java
@@ -0,0 +1,26 @@
+class Person{
+    String name;
+    int age;
+    boolean isMale;
+
+    void setName(String name){
+        this.name = name;
+    }
+    void setAge(int age){
+       this.age = age;
+    }
+    void setGender(boolean isMale){
+        this.isMale = isMale;
+    }
+
+    String getName(){
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
+    boolean getGender(){
+        return isMale;
+    }
+
+} 
